859a51d5917f49b202c0b97c2ce1a40bbbe54e77
Hiitagiii <mirzaspc@gmail.com>
2021-04-07 11:26:53 +0000

    surya: remove more packages
    Signed-off-by: Hiitagiii <mirzaspc@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Photos Youtube YoutubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := arcore CalculatorGooglePrebuilt CalendarGooglePrebuilt Chrome Chrome-Stub DiagnosticsToolPrebuilt Drive Maps MicropaperPrebuilt NgaResources Ornament PrebuiltGmail PrebuiltGoogleTelemetryTvp SoundAmplifierPrebuilt Photos RecorderPrebuilt SCONE SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM talkback Tycho VZWAPNLib WallpapersBReel2020 Youtube YoutubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
