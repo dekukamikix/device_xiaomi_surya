cafe1850dbf556efbc9ffd668e33402ec3e8a84c
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    surya: Remove some Gapps packages at build time
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
