c453074e8161e796721a0aebba4a3b46251dac62
Nauval Rizky <enuma.alrizky@gmail.com>
2021-07-02 19:24:28 +0700

    surya: BoardConfig: add LLVM=1 flags
    Since our kernel already support LLVM=1 switch just like mainline.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := silont
 TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
